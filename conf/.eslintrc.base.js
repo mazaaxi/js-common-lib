4371e0778a024fd9b36d6be095f65228fba98f7a
masterpg <masterpg@livedeveloper.net>
2020-03-18 23:07:25 +0900

    eslintの`@typescript-eslint/no-empty-interface`をoffに変更

--- conf/.eslintrc.base.js
+++ conf/.eslintrc.base.js
@@ -71,6 +71,7 @@ module.exports = {
       },
     ],
     '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-namespace': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
