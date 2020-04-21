18531c2ab74d95a09fe7a7d691ac7c39d771da17
masterpg <masterpg@livedeveloper.net>
2020-04-21 17:01:05 +0900

    TypeScriptの新たな型として`Require`と`Optional`を追加

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,1 +1,3 @@
 export declare type Constructor<T = any> = new (...args: any[]) => T;
+export declare type Require<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>;
+export declare type Optional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,5 @@
 export type Constructor<T = any> = new (...args: any[]) => T
+
+export type Require<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>
+
+export type Optional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
