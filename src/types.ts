b156a01269194a5870da1ef5dc5a7a5a3bc86690
masterpg <masterpg@livedeveloper.net>
2020-06-03 18:11:25 +0900

    TypeScriptの新たな型として`DeepPartial`を追加

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,13 @@ export type Constructor<T = any> = new (...args: any[]) => T
 export type AreRequired<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>
 
 export type AreOptional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
+
+export type DeepPartial<T> = {
+  [K in keyof T]?: T[K] extends null
+    ? null
+    : T[K] extends undefined
+    ? undefined
+    : T[K] extends Array<infer R>
+    ? Array<DeepPartial<R>>
+    : DeepPartial<T[K]>
+}
