4fc70786f87bffbb0175a49c9c778254222433a2
masterpg <masterpg@livedeveloper.net>
2020-07-01 16:46:30 +0900

    型の名前を変更

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,6 +1,6 @@
 export declare type Constructor<T = any> = new (...args: any[]) => T;
-export declare type AreRequired<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>;
-export declare type AreOptional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+export declare type RequiredAre<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>;
+export declare type PartialAre<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
 export declare type DeepPartial<T> = {
     [K in keyof T]?: T[K] extends null ? null : T[K] extends undefined ? undefined : T[K] extends Array<infer R> ? Array<DeepPartial<R>> : DeepPartial<T[K]>;
 };
--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,8 @@
 export type Constructor<T = any> = new (...args: any[]) => T
 
-export type AreRequired<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>
+export type RequiredAre<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>
 
-export type AreOptional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
+export type PartialAre<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
 
 export type DeepPartial<T> = {
   [K in keyof T]?: T[K] extends null
