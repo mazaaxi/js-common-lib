ea35ec98a551b73f47185574ed2893a62f6af4e3
masterpg <masterpg@livedeveloper.net>
2020-11-29 00:23:14 +0900

    eslintの`@typescript-eslint/explicit-module-boundary-types`をoffにする設定を追加

--- conf/.eslintrc.base.js
+++ conf/.eslintrc.base.js
@@ -59,6 +59,7 @@ module.exports = {
     '@typescript-eslint/camelcase': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/indent': ['off', 2],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/member-delimiter-style': [
