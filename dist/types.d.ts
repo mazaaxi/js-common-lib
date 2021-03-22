68176ecf21f7fa0b5eb6d0497278db9b8814b5fc
masterpg <masterpg@livedeveloper.net>
2021-03-22 15:07:33 +0900

    `DeepPartial`と`DeepReadonly`を改善

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,9 +1,10 @@
+import { Dayjs } from 'dayjs';
 export declare type Constructor<T = any> = new (...args: any[]) => T;
 export declare type RequiredAre<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>;
 export declare type PartialAre<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
 export declare type DeepPartial<T> = {
-    [K in keyof T]?: T[K] extends null ? null : T[K] extends undefined ? undefined : T[K] extends Function ? T[K] : T[K] extends Array<infer R> ? Array<DeepPartial<R>> : DeepPartial<T[K]>;
+    [K in keyof T]?: T[K] extends undefined ? undefined : T[K] extends null ? null : T[K] extends Function ? T[K] : T[K] extends Dayjs ? T[K] : T[K] extends Record<any, any> ? DeepPartial<T[K]> : T[K] extends Array<infer R> ? Array<DeepPartial<R>> : T[K];
 };
 export declare type DeepReadonly<T> = {
-    readonly [K in keyof T]: T[K] extends null ? null : T[K] extends undefined ? undefined : T[K] extends Function ? T[K] : T[K] extends Array<infer R> ? Array<DeepReadonly<R>> : DeepReadonly<T[K]>;
+    readonly [K in keyof T]: T[K] extends undefined ? undefined : T[K] extends null ? null : T[K] extends Function ? T[K] : T[K] extends Dayjs ? T[K] : T[K] extends Record<any, any> ? DeepReadonly<T[K]> : T[K] extends Array<infer R> ? Array<DeepReadonly<R>> : T[K];
 };
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+import { Dayjs } from 'dayjs'
+
 export type Constructor<T = any> = new (...args: any[]) => T
 
 export type RequiredAre<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>
@@ -5,25 +7,33 @@ export type RequiredAre<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>
 export type PartialAre<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
 
 export type DeepPartial<T> = {
-  [K in keyof T]?: T[K] extends null
-    ? null
-    : T[K] extends undefined
+  [K in keyof T]?: T[K] extends undefined
     ? undefined
+    : T[K] extends null
+    ? null
     : T[K] extends Function
     ? T[K]
+    : T[K] extends Dayjs
+    ? T[K]
+    : T[K] extends Record<any, any>
+    ? DeepPartial<T[K]>
     : T[K] extends Array<infer R>
     ? Array<DeepPartial<R>>
-    : DeepPartial<T[K]>
+    : T[K]
 }
 
 export type DeepReadonly<T> = {
-  readonly [K in keyof T]: T[K] extends null
-    ? null
-    : T[K] extends undefined
+  readonly [K in keyof T]: T[K] extends undefined
     ? undefined
+    : T[K] extends null
+    ? null
     : T[K] extends Function
     ? T[K]
+    : T[K] extends Dayjs
+    ? T[K]
+    : T[K] extends Record<any, any>
+    ? DeepReadonly<T[K]>
     : T[K] extends Array<infer R>
     ? Array<DeepReadonly<R>>
-    : DeepReadonly<T[K]>
+    : T[K]
 }
