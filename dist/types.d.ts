dd6917d0d70e9c46aa612b0d3caaf9d08332be81
masterpg <masterpg@livedeveloper.net>
2021-05-07 14:06:56 +0900

    `Overwrite`型と`ReplaceType`型を追加

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,17 +1,50 @@
 import { Dayjs } from 'dayjs';
-export declare type Constructor<T = any> = new (...args: any[]) => T;
-export declare type RequiredAre<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>;
-export declare type PartialAre<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
-export declare type DeepPartial<T> = {
+declare type Constructor<T = any> = new (...args: any[]) => T;
+declare type RequiredAre<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>;
+declare type PartialAre<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+declare type DeepPartial<T> = {
     [K in keyof T]?: T[K] extends undefined ? undefined : T[K] extends null ? null : T[K] extends Function ? T[K] : T[K] extends Function | undefined ? T[K] : T[K] extends Dayjs ? T[K] : T[K] extends Dayjs | undefined ? T[K] : T[K] extends Record<any, any> ? DeepPartial<T[K]> : T[K] extends Array<infer R> ? Array<DeepPartial<R>> : DeepPartial<T[K]>;
 };
-export declare type DeepReadonly<T> = {
+declare type DeepReadonly<T> = {
     readonly [K in keyof T]: T[K] extends undefined ? undefined : T[K] extends null ? null : T[K] extends Function ? T[K] : T[K] extends Function | undefined ? T[K] : T[K] extends Dayjs ? T[K] : T[K] extends Dayjs | undefined ? T[K] : T[K] extends Record<any, any> ? DeepReadonly<T[K]> : T[K] extends Array<infer R> ? Array<DeepReadonly<R>> : DeepReadonly<T[K]>;
 };
-export declare type ToNull<T> = T extends undefined ? null : T;
-export declare type ToDeepNull<T> = {
+declare type ToNull<T> = T extends undefined ? null : T;
+declare type ToDeepNull<T> = {
     [K in keyof T]: T[K] extends Record<any, any> ? ToDeepNull<T[K]> : T[K] extends Record<any, any> | undefined ? ToDeepNull<T[K]> | null : ToNull<T[K]>;
 };
-export declare type ToStrictDeepNull<T> = {
+declare type ToStrictDeepNull<T> = {
     [K in keyof T]-?: T[K] extends Record<any, any> ? ToStrictDeepNull<T[K]> : T[K] extends Record<any, any> | undefined ? ToStrictDeepNull<T[K]> | null : ToNull<T[K]>;
 };
+/**
+ * `TARGET`型に`SOURCE`型を上書きします。
+ *
+ * @example
+ * type Target = { name: string; age: number }
+ * type Source = { name: string; age?: number; weight: number }
+ * type Person = Overwrite<Target, Source>
+ * const p: Person = null as any
+ * p.name = 'Taro' // string
+ * p.age = undefined // number | undefined
+ * p.weight = 170 // number
+ */
+declare type Overwrite<TARGET, SOURCE> = Omit<TARGET, keyof SOURCE> & SOURCE;
+declare type ReplaceType1<T, S> = {
+    [K in keyof T]: T[K] extends S ? unknown : T[K];
+};
+declare type ReplaceType2<T, R> = {
+    [K in keyof T]: T[K] extends unknown ? R : T[K];
+};
+/**
+ * `T`型の中で`S`型のメンバーを`R`型に置き換えます。
+ *
+ * @example
+ * type Article = { title: string; createdAt: Date; updatedAt: Date }
+ * type RawArticle = ReplaceType<Article, Date, string>
+ * const article: RawArticle = {
+ *   title: `Today's events`, // string型
+ *   createdAt: '2021-01-01T01:15:00.000Z', // Date → string
+ *   updatedAt: '2021-01-01T01:20:00.000Z', // Date → string
+ * }
+ */
+declare type ReplaceType<T, S, R> = ReplaceType1<T, S> & ReplaceType2<T, R>;
+export { Constructor, DeepPartial, DeepReadonly, Overwrite, PartialAre, ReplaceType, RequiredAre, ToDeepNull, ToNull, ToStrictDeepNull };
--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,18 @@
 import { Dayjs } from 'dayjs'
 
-export type Constructor<T = any> = new (...args: any[]) => T
+//========================================================================
+//
+//  Interfaces
+//
+//========================================================================
 
-export type RequiredAre<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>
+type Constructor<T = any> = new (...args: any[]) => T
 
-export type PartialAre<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
+type RequiredAre<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>
 
-export type DeepPartial<T> = {
+type PartialAre<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
+
+type DeepPartial<T> = {
   [K in keyof T]?: T[K] extends undefined
     ? undefined
     : T[K] extends null
@@ -26,7 +32,7 @@ export type DeepPartial<T> = {
     : DeepPartial<T[K]>
 }
 
-export type DeepReadonly<T> = {
+type DeepReadonly<T> = {
   readonly [K in keyof T]: T[K] extends undefined
     ? undefined
     : T[K] extends null
@@ -46,9 +52,9 @@ export type DeepReadonly<T> = {
     : DeepReadonly<T[K]>
 }
 
-export type ToNull<T> = T extends undefined ? null : T
+type ToNull<T> = T extends undefined ? null : T
 
-export type ToDeepNull<T> = {
+type ToDeepNull<T> = {
   [K in keyof T]: T[K] extends Record<any, any>
     ? ToDeepNull<T[K]>
     : T[K] extends Record<any, any> | undefined
@@ -56,10 +62,53 @@ export type ToDeepNull<T> = {
     : ToNull<T[K]>
 }
 
-export type ToStrictDeepNull<T> = {
+type ToStrictDeepNull<T> = {
   [K in keyof T]-?: T[K] extends Record<any, any>
     ? ToStrictDeepNull<T[K]>
     : T[K] extends Record<any, any> | undefined
     ? ToStrictDeepNull<T[K]> | null
     : ToNull<T[K]>
 }
+
+/**
+ * `TARGET`型に`SOURCE`型を上書きします。
+ *
+ * @example
+ * type Target = { name: string; age: number }
+ * type Source = { name: string; age?: number; weight: number }
+ * type Person = Overwrite<Target, Source>
+ * const p: Person = null as any
+ * p.name = 'Taro' // string
+ * p.age = undefined // number | undefined
+ * p.weight = 170 // number
+ */
+type Overwrite<TARGET, SOURCE> = Omit<TARGET, keyof SOURCE> & SOURCE
+
+type ReplaceType1<T, S> = {
+  [K in keyof T]: T[K] extends S ? unknown : T[K]
+}
+type ReplaceType2<T, R> = {
+  [K in keyof T]: T[K] extends unknown ? R : T[K]
+}
+
+/**
+ * `T`型の中で`S`型のメンバーを`R`型に置き換えます。
+ *
+ * @example
+ * type Article = { title: string; createdAt: Date; updatedAt: Date }
+ * type RawArticle = ReplaceType<Article, Date, string>
+ * const article: RawArticle = {
+ *   title: `Today's events`, // string型
+ *   createdAt: '2021-01-01T01:15:00.000Z', // Date → string
+ *   updatedAt: '2021-01-01T01:20:00.000Z', // Date → string
+ * }
+ */
+type ReplaceType<T, S, R> = ReplaceType1<T, S> & ReplaceType2<T, R>
+
+//========================================================================
+//
+//  Exports
+//
+//========================================================================
+
+export { Constructor, DeepPartial, DeepReadonly, Overwrite, PartialAre, ReplaceType, RequiredAre, ToDeepNull, ToNull, ToStrictDeepNull }
